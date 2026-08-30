3c71eccf99393cfc3f8998c656f1b9ae9b1ecc66
Sakthi Praveen <sakthipraveen2007@gmail.com>
2026-08-31 00:15:51 +0530

    Connect portfolio to online backend

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("contactForm").addEventListener("submit", async function
 
     try {
 
-        const response = await fetch("http://localhost:8080/contact", {
+        fetch("https://sakthi-praveen-portfolio-production.up.railway.app/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
