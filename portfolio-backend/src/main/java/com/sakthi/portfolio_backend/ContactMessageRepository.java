015d16a860bfab7f55439b7298782783629bd419
sakthipraveen-01 <sakthipraveen2007@gmail.com>
2026-09-06 18:05:46 +0530

    Delete portfolio-backend directory

--- portfolio-backend/src/main/java/com/sakthi/portfolio_backend/ContactMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sakthi.portfolio_backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactMessageRepository
-        extends JpaRepository<ContactMessage, Long> {
-}
